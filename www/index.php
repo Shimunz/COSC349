75969246081276057f2185dbd10e77404eb09fe0
808sand404s <nztimothy221@gmail.com>
2020-08-26 16:41:02 +1200

    Added files

--- www/index.php
+++ www/index.php
@@ -1,11 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
-<head>
-<title>World Timezone Converter</title>
+<head><title>World Timezone Converter</title>
+  <style>
+    th { text-align: left; }
+</style>
 </head>
 
 <body>
-  <p><?php $time = file_get_contents("192.168.11.11?timezone=nzt");?></p>
+  <h1>World Timezone Converter</h1>
+   <?php
+    $time = file_get_contents('192.168.11.11?timezone=nzt');
+    ?>
   <p>The current local time is: <?php echo "$time";?></p>
-  <p>HELP</p>
 </body>
 </html>
